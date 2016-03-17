ebc9ad1acc0b38ca73792f95ab7a11045b14b416
Mark Titorenko <mark.titorenko@alces-software.com>
2016-03-17 19:11:33 +0000

    wip asdlkjasd

--- .travis/build-container.sh
+++ .travis/build-container.sh
@@ -3,6 +3,8 @@ set -ex
 sudo service docker stop
 sudo apt-get update
 sudo apt-get -y -o Dpkg::Options::=--force-confdef -o Dpkg::Options::="--force-confnew" install docker-engine
-docker build --build-arg treeish=${TRAVIS_COMMIT} \
+REPO="${TRAVIS_REPO_SLUG}"
+REPO="alces-software/packager-base"
+docker build --build-arg treeish=${TRAVIS_COMMIT} --build-arg repo_slug="${REPO}" \
        -t "alces/packages-${TRAVIS_COMMIT}-${cw_DIST}-${cw_VERSION}" \
        ".travis/${cw_DIST}-${cw_VERSION}"
--- .travis/deploy-builds.sh
+++ .travis/deploy-builds.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+set -x
 if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
+    env | grep ARTIFACTS
     export ARTIFACTS_REGION='eu-west-1'
-    artifacts upload \
+    artifacts -f multiline upload \
 	      --permissions=public-read \
 	      --target-paths 'gridware' \
 	      --working-dir "$HOME" \
