2b11ea3d682a9114e8f93957f32f8bee3181a05f
Mark Titorenko <mark.titorenko@alces-software.com>
2016-03-17 19:03:30 +0000

    foobar 989

--- .travis/deploy-builds.sh
+++ .travis/deploy-builds.sh
@@ -2,7 +2,7 @@
 if [ "$TRAVIS_BRANCH" == "master" -a "$TRAVIS_PULL_REQUEST" == "false" ]; then
     export ARTIFACTS_REGION='eu-west-1'
     artifacts upload \
-	      --permissions=public \
+	      --permissions=public-read \
 	      --target-paths 'gridware' \
 	      --working-dir "$HOME" \
 	      '$dist'
